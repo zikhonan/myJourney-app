9c8e89eb3e36d2e0884d4bdd8389a1fb58346286
zikhona <precious.nqula@gmail.com>
2018-11-11 15:57:30 +0200

    updates

--- Assignment03/sw.js
+++ Assignment03/sw.js
@@ -1,87 +1,32 @@
-self.addEventListener('install', function(e) {
-    console.log('[Service Worker] Install');
-});
+const CACHE_NAME = 'myJourney-app-site-cache-v1'
+const urlsToCache = [
+  '/',
+  '/bulma.css',
+  '/index.html',
+  '/main.js',
+]
 
-var cacheName = 'js13kPWA-v1';
-var appShellFiles = [
-  '/pwa-examples/js13kpwa/',
-  '/pwa-examples/js13kpwa/index.html',
-  '/pwa-examples/js13kpwa/app.js',
-  '/pwa-examples/js13kpwa/style.css',
-  '/pwa-examples/js13kpwa/fonts/graduate.eot',
-  '/pwa-examples/js13kpwa/fonts/graduate.ttf',
-  '/pwa-examples/js13kpwa/fonts/graduate.woff',
-  '/pwa-examples/js13kpwa/favicon.ico',
-  '/pwa-examples/js13kpwa/img/js13kgames.png',
-  '/pwa-examples/js13kpwa/img/bg.png',
-  '/pwa-examples/js13kpwa/icons/icon-32.png',
-  '/pwa-examples/js13kpwa/icons/icon-64.png',
-  '/pwa-examples/js13kpwa/icons/icon-96.png',
-  '/pwa-examples/js13kpwa/icons/icon-128.png',
-  '/pwa-examples/js13kpwa/icons/icon-168.png',
-  '/pwa-examples/js13kpwa/icons/icon-192.png',
-  '/pwa-examples/js13kpwa/icons/icon-256.png',
-  '/pwa-examples/js13kpwa/icons/icon-512.png'
-];
-
-var myJourney = [];
-for(var i=0; i<myJourney.length; i++) {
-  myJourney.push('data/img/'+myJourney[i].slug+'.jpg');
+const install = event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  )
 }
-var contentToCache = appShellFiles.concat(myJourney);
 
-self.addEventListener('install', function(e) {
-    console.log('[Service Worker] Install');
-    e.waitUntil(
-      caches.open(cacheName).then(function(cache) {
-            console.log('[Service Worker] Caching all: app shell and content');
-        return cache.addAll(contentToCache);
-      })
-    );
-  });
-
-  self.addEventListener('fetch', function(e) {
-    console.log('[Service Worker] Fetched resource '+e.request.url);
-});
-
-self.addEventListener('fetch', function(e) {
-    e.respondWith(
-      caches.match(e.request).then(function(r) {
-            console.log('[Service Worker] Fetching resource: '+e.request.url);
-        return r || fetch(e.request).then(function(response) {
-                  return caches.open(cacheName).then(function(cache) {
-            console.log('[Service Worker] Caching new resource: '+e.request.url);
-            cache.put(e.request, response.clone());
-            return response;
-          });
-        });
-      })
-    );
-  });
-
-  var cacheName = 'js13kPWA-v1';
-
-  contentToCache.push('/pwa-examples/js13kpwa/icons/icon-32.png');
-
-// ...
-
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('js13kPWA-v2').then(function(cache) {
-      return cache.addAll(contentToCache);
+const fetchData = event => {
+  event.respondWith(
+    caches.open(CACHE_NAME).then( cache => {
+      const getFromServer = () => 
+        fetch(event.request)
+          .then(networkResponse => {
+            cache.put(event.request, networkResponse.clone())
+            return networkResponse
+          })
+      getFromServer()
+      return caches.match(event.request).catch(() => getFromServer())
     })
-  );
-});
+  )
+}
 
-self.addEventListener('activate', function(e) {
-    e.waitUntil(
-      caches.keys().then(function(keyList) {
-            return Promise.all(keyList.map(function(key) {
-          if(cacheName.indexOf(key) === -1) {
-            return caches.delete(key);
-          }
-        }));
-      })
-    );
-  });
-
+self.addEventListener('install', install)
+self.addEventListener('fetch', fetchData)
