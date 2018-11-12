d7ba165a248e3a2a485e3c074d31c64d2c0b2822
zikhona <precious.nqula@gmail.com>
2018-11-12 15:56:20 +0200

    update

--- Assignment03/main.js
+++ Assignment03/main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function registerServiceWorker() {
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker.register('/sw.js').then(reg => {
@@ -15,3 +16,37 @@ function registerServiceWorker() {
   }
   
   window.addEventListener('load', load)
+=======
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js').then(function(reg) {
+      console.log('Successfully registered service worker',reg);      
+    }).catch(function(err) {
+      console.warn('Error whilst registering service worker',err);
+    });
+};
+
+window.addEventListener('online', function(e) {
+  // Resync data with server.
+  console.log("You are online");
+  Page.hideOfflineWarning();
+  Arrivals.loadData();
+}, false);
+
+window.addEventListener('offline', function(e) {
+  // Queue up events for server.
+  console.log("You are offline");
+  Page.showOfflineWarning();
+}, false);
+
+// Check if the user is connected.
+if (navigator.onLine) {
+  Arrivals.loadData();
+} else {
+  // Show offline message
+  Page.showOfflineWarning();
+}
+
+// Set Knockout view model bindings.
+ko.applyBindings(Page.vm);
+  
+>>>>>>> 4396b3912e290a3285d426828bf4df6d46aa2867
