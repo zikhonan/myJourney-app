2fb88470e33d81c68964624953949d841d91a1d0
Zikhona <precious.nqula@gmail.com>
2018-11-13 14:10:52 +0200

    updates

--- form with validation/script.js
+++ form with validation/script.js
@@ -5,30 +5,28 @@ var app =new Vue({
         password:'',
         usernameError: false,
         passwordError: false,
-        usernameErrorMessage:''
+        usernameErrorMessage:[]
     },
     methods: {
         enter: function() {
         //check if passowrd is longer than 6 digits.
         this.usernameError =false
         this.passwordError =false
-        this.usernameErrorMessage =''
-        this.passwordErrorMessage =''
+        this.usernameErrorMessage =[]        
 
         if(this.password.length < 6){ 
             this.passwordError =true
-            this.passwordErrorMessage ='password is too short'
             
         }
         
         if(this.username.length <3) {
             this.usernameError = true
-            this.usernameErrorMessage = 'Username is too short'
+            this.usernameErrorMessage.push ({msg: 'Username is too short' ,date: Date.now()})
             //alert('You have entered less than 3 characters for username');
         }
         if(!this.username.includes('0')) {
             this.usernameError = true
-            this.usernameErrorMessage ='Username must be a vilid email address'
+            this.usernameErrorMessage.push ({msg:'Username must be a vilid email address',date: Date.now()})
         }
     }
     }
