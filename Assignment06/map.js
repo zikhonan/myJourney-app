88264ebb483809a392cea3eaff74ee07f5e72353
Zikhona <precious.nqula@gmail.com>
2018-11-14 17:25:10 +0200

    mapbox with coordinates

---
+++ Assignment06/map.js
@@ -0,0 +1,6 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiemlraG9uYW4iLCJhIjoiY2ptdDhmaGczMTd1czNrbDg5MTRoZXc3YSJ9.E-GocNkDS6sjpjBdpk4Tdg';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v10'
+});
+
